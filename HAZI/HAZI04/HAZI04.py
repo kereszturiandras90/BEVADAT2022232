c929953e3cd6696df882aeaa9df9d4c61593f89b
andras <kereszturiandras90@gmail.com>
2023-03-25 22:46:43 +0100

    add grad

--- HAZI/HAZI04/HAZI04.py
+++ HAZI/HAZI04/HAZI04.py
@@ -70,3 +70,19 @@ def female_top_score(df):
     return ered
 
 print(female_top_score(csv_to_df("StudentsPerformance.csv"))) 
+
+
+def add_grade(df):
+    new_df = df.copy()
+    point_to_grad = {(0.00, 0.59): "F", (0.60, 0.69): "D", (0.70, 0.79): "C", (0.80, 0.89): "B", (0.90, 1.00): "A"}
+    def grade_range(x):
+     for key in point_to_grad:
+        if x >= key[0] and x <= key[1]:
+            return point_to_grad[key]
+      
+
+    new_df['grade']=((new_df['reading score'] + new_df['writing score'] + new_df['math score'] ) / 300)
+    new_df['grade']=new_df['grade'].apply(grade_range)
+    return new_df
+
+print(add_grade(csv_to_df("StudentsPerformance.csv")))     
