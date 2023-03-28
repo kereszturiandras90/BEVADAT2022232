781bde251104c6cd4541be89fcd464e12bf8b73b
andras <kereszturiandras90@gmail.com>
2023-03-28 21:23:01 +0200

    corr

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -4,7 +4,7 @@ from typing import Tuple
 from scipy.stats import mode
 from sklearn.metrics import confusion_matrix
 
-csv_path = "iris.data.csv"
+csv_path = "iris.csv"
 
 class KNNClassifier:
     k_neighbors = 0
@@ -22,7 +22,7 @@ class KNNClassifier:
      x,y = dataset[:,:4],dataset[:,-1]
      return x,y   
 
-    def train_test_split(features:np.ndarray, labels:np.ndarray):
+    def train_test_split(self, features:np.ndarray, labels:np.ndarray):
         
      test_size = int(len(features) * self.test_split_ratio)
      train_size = len(features) - test_size
@@ -31,10 +31,10 @@ class KNNClassifier:
      self.x_train,self.y_train = features[:train_size,:],labels[:train_size]
      self.x_test,self.y_test = features[train_size:train_size+test_size,:], labels[train_size:train_size + test_size]
 
-    def euclidean(element_of_x:np.ndarray) -> np.ndarray:
+    def euclidean(self, element_of_x:np.ndarray) -> np.ndarray:
      return np.sqrt(np.sum((self.x_train - element_of_x)**2,axis=1))
 
-    def predict(x_test:np.ndarray) -> np.ndarray:
+    def predict(self, x_test:np.ndarray) -> np.ndarray:
      labels_pred = []
      for x_test_element in x_test:
         distances = euclidean(x_test_element)
@@ -44,18 +44,16 @@ class KNNClassifier:
      self.y_preds = np.array(labels_pred,dtype=np.int32)   
      return np.array(labels_pred,dtype=np.int32)
 
-    def accuracy() -> float:
+    def accuracy(self) -> float:
      true_positive = (self.y_test == self.y_preds).sum()
      return true_positive / len(self.y_test) * 100
 
-    def confusion_matrix():
+    def confusion_matrix(self):
         conf_matrix = confusion_matrix(self._test,self.y_preds)
         sns.heatmap(conf_matrix,annot=True)
  
 
     
-
-
 
 
 
