076ecf2cea7d0c7a937bfd7daca8b2d1b3f019c0
andras <kereszturiandras90@gmail.com>
2023-04-01 23:18:32 +0200

    GYAK05 new

--- GYAK/GYAK05/GYAK05.py
+++ GYAK/GYAK05/GYAK05.py
@@ -20,16 +20,17 @@ class KNNClassifier:
      dataset = np.genfromtxt(csv_path,delimiter=',')
      np.random.shuffle(dataset,)
      x,y = dataset[:,:4],dataset[:,-1]
-     return x,y   
+     return x,y  
 
-    def train_test_split(self, features:np.ndarray, labels:np.ndarray):
+    def train_test_split(self, features: np.array, labels: np.array) -> tuple:
         
      test_size = int(len(features) * self.test_split_ratio)
      train_size = len(features) - test_size
      assert len(features) == test_size + train_size, "Size mismatch!"
 
-     self.x_train,self.y_train = features[:train_size,:],labels[:train_size]
-     self.x_test,self.y_test = features[train_size:train_size+test_size,:], labels[train_size:train_size + test_size]
+     self.x_train, self.y_train = features[:train_size,:], labels[:train_size]
+     self.x_test, self.y_test = features[train_size:train_size+test_size,:], labels[train_size:train_size+test_size]
+     return (self.x_train, self.y_train, self.x_test, self.y_test)
 
     def euclidean(self, element_of_x:np.ndarray) -> np.ndarray:
      return np.sqrt(np.sum((self.x_train - element_of_x)**2,axis=1))
@@ -37,7 +38,7 @@ class KNNClassifier:
     def predict(self, x_test:np.ndarray) -> np.ndarray:
      labels_pred = []
      for x_test_element in x_test:
-        distances = euclidean(self, x_test_element)
+        distances = self.euclidean(x_test_element)
         distances = np.array(sorted(zip(distances,self.y_train)))
         label_pred = mode(distances[:self.k,1],keepdims=False).mode
         labels_pred.append(label_pred)
@@ -50,13 +51,9 @@ class KNNClassifier:
 
     def confusion_matrix(self) -> np.ndarray:
         conf_matrix = confusion_matrix(self.y_test,self.y_preds)
-        #sns.heatmap(conf_matrix,annot=True)
         return conf_matrix
 
-p1 = KNNClassifier(0.5,0.5)
-x, y = p1.load_csv("diabetes.csv")
-#    x, y = load_csv("diabetes.csv")
-print(p1.train_test_split(x,y))        
+ 
  
 
     
