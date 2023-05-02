04083b154400dbc636a343a72995eea05a108bc0
andras <kereszturiandras90@gmail.com>
2023-05-02 16:01:52 +0200

    fix hf

--- HAZI/HAZI09/HAZI09.py
+++ HAZI/HAZI09/HAZI09.py
@@ -1,99 +1,32 @@
-# %%
-# imports
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
 import numpy as np
+from sklearn.datasets import load_digits
 from sklearn.cluster import KMeans
-from sklearn.metrics import accuracy_score
-from scipy.stats import mode
 from sklearn.metrics import confusion_matrix
 
-# %%
-# Készíts egy függvényt ami betölti a digits datasetet 
-# NOTE: használd az sklearn load_digits-et
-# Függvény neve: load_digits()
-# Függvény visszatérési értéke: a load_digits visszatérési értéke
-from sklearn.datasets import load_digits as sk_load_digits
+class KMeansOnDigits:
 
-def load_digits():
-    return sk_load_digits()
+    def __init__(self, n_clusters, random_state):
+        self.n_clusters = n_clusters
+        self.random_state=random_state
 
-# %%
-# Vizsgáld meg a betöltött adatszetet (milyen elemek vannak benne stb.)
+    def load_dataset(self):
+        self.digits = load_digits()
 
-dataset = load_digits()
-print(dataset.feature_names)
-print(dataset.target_names)
-print(dataset.target)
+    def predict(self):
+        self.model = KMeans(n_clusters=self.n_clusters, random_state=self.random_state)
+        self.clusters = self.model.fit_predict(self.digits.data)
 
-# %%
-# Vizsgáld meg a data paraméterét a digits dataset-nek (tartalom,shape...)
-print(dataset.data.shape)
-print(dataset.data)
+    def get_labels(self):
+        l = np.zeros_like(self.clusters)
+        for c in range(self.n_clusters):
+            mask = (self.clusters == c)
+            target = self.digits.target[mask]
+            mode = np.bincount(target).argmax()
+            l[mask] = mode
+        self.labels = l
 
-# %%
-# Készíts egy függvényt ami létrehoz egy KMeans model-t 10 db cluster-el
-# NOTE: használd az sklearn Kmeans model-jét (random_state legyen 0)
-# Miután megvan a model predict-elj vele 
-# NOTE: használd a fit_predict-et
-# Függvény neve: predict(n_clusters:int,random_state:int,digits)
-# Függvény visszatérési értéke: (model:sklearn.cluster.KMeans,clusters:np.ndarray)
-def predict(n_clusters:int,random_state:int,dataset):
-    model = KMeans(n_clusters=n_clusters, random_state=random_state)
-    clusters = model.fit_predict(dataset.data, dataset.target)
-    return model, clusters
+    def calc_accuracy(self):
+        self.accuracy = round(np.mean(self.labels == self.digits.target), 2)
 
-
-model, clusters = predict(10, 0, dataset)
-
-# %%
-# Vizsgáld meg a shape-jét a kapott model cluster_centers_ paraméterének.
-print(model.cluster_centers_.shape)
-
-# %%
-# Készíts egy plotot ami a cluster középpontokat megjeleníti
-plt.scatter(model.cluster_centers_[:, 0], model.cluster_centers_[:, 1])
-
-# %%
-# Készíts egy függvényt ami visszaadja a predictált cluster osztályokat
-# NOTE: amit a predict-ből visszakaptunk "clusters" azok lesznek a predictált cluster osztályok
-# HELP: amit a model predictált cluster osztályok még nem a labelek, hanem csak random cluster osztályok, 
-#       Hogy label legyen belőlük:
-#       1. készíts egy result array-t ami ugyan annyi elemű mint a predictált cluster array
-#       2. menj végig mindegyik cluster osztályon (0,1....9)
-#       3. készíts egy maszkot ami az adott cluster osztályba tartozó elemeket adja vissza
-#       4. a digits.target-jét indexeld meg ezzel a maszkkal
-#       5. számold ki ennel a subarray-nek a móduszát
-#       6. a result array-ben tedd egyenlővé a módusszal azokat az indexeket ahol a maszk True 
-#       Erre azért van szükség mert semmi nem biztosítja nekünk azt, hogy a "0" cluster a "0" label lesz, lehet, hogy az "5" label lenne az.
-
-# Függvény neve: get_labels(clusters:np.ndarray, digits)
-# Függvény visszatérési értéke: labels:np.ndarray
-def get_labels(clusters:np.ndarray, dataset)-> np.ndarray:
-    labels = np.empty(shape=clusters.shape)
-
-    for i in dataset.target_names:
-        mask = clusters == i
-        labels[mask] = mode(dataset.target[mask], keepdims=False).mode
-    
-    return labels
-
-        
-labels = get_labels(clusters, dataset)
-
-# %%
-# Készíts egy függvényt ami kiszámolja a model accuracy-jét
-# Függvény neve: calc_accuracy(target_labels:np.ndarray,predicted_labels:np.ndarray)
-# Függvény visszatérési értéke: accuracy:float
-# NOTE: Kerekítsd 2 tizedes jegyre az accuracy-t
-def calc_accuracy(target_labels:np.ndarray,predicted_labels:np.ndarray):
-    return np.round(accuracy_score(target_labels, predicted_labels), 2)
-
-calc_accuracy(dataset.target, labels)
-
-# %%
-# Készíts egy confusion mátrixot és plot-old seaborn segítségével
-conf_matrix = confusion_matrix(dataset.target, labels)
-
-sns.heatmap(conf_matrix)
+    def confusion_matrix(self):
+        self.mat = confusion_matrix(self.digits.target, self.labels)
